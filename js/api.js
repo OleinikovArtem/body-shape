e5df05bd6c9a209563e010f3519142d0c10cd6b7
Bignichok <62079994+Bignichok@users.noreply.github.com>
2024-11-07 20:52:51 +0000

    Deploying to gh-pages from @ OleinikovArtem/body-shape@c417a6defabc665f7e7440b43a12c415f7f7f059 🚀

--- js/api.js
+++
@@ -1,2 +0,0 @@
-import{a as n}from"../assets/vendor-CNNbG8jS.js";const e={BASE_URL:"https://your-energy.b.goit.study/api",EXERCISES:"/exercises",FILTERS:"/filters",QUOTE:"/quote",SUBSCRIPTION:"/subscription",RATING:"/rating"};async function i(t){const s=u(e.SUBSCRIPTION);return o(s,{email:t})}async function o(t,s){return n.post(t,s).then(r=>r).catch(r=>r.response)}function u(t,s){return e.BASE_URL+t+""}export{i as s};
-//# sourceMappingURL=api.js.map
