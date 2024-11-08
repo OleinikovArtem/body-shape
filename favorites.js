d3dee8dda32889b5f721ae2dbe0bae8eb4be0d43
OleinikovArtem <38537243+OleinikovArtem@users.noreply.github.com>
2024-11-08 18:27:46 +0000

    Deploying to gh-pages from @ OleinikovArtem/body-shape@aa21ec09cc06b3a90c7c0ffa99e3f7541de8ed18 🚀

--- favorites.js
+++ favorites.js
@@ -1,2 +1,11 @@
-import{g as u}from"./assets/api-CEZt36uo.js";import"./assets/vendor-BT7OT44q.js";async function c(){try{const t=await u();if(console.log("Quote response:",t),t){const n=t.quote,r=t.author,e=document.querySelector(".quote-text"),o=document.querySelector(".quote-author");e&&o&&(e.textContent=n,o.textContent=`— ${r}`)}else console.log("Failed to load quote of the day.")}catch(t){console.error("Error fetching quote of the day:",t)}}document.addEventListener("DOMContentLoaded",c);
+import{g as c,a as d}from"./assets/api-CTkoU--0.js";import"./assets/vendor-BT7OT44q.js";async function u(){try{const e=await c();if(e){const t=e.quote,r=e.author,s=document.querySelector(".quote-text"),o=document.querySelector(".quote-author");s&&o&&(s.textContent=t,o.textContent=`${r}`)}else console.log("Failed to load quote of the day.")}catch(e){console.error("Error fetching quote of the day:",e)}}document.addEventListener("DOMContentLoaded",u);function i(){const e=localStorage.getItem("favoriteExercises");return e?JSON.parse(e):[]}async function n(){const e=document.querySelector(".favorites-list"),t=i();if(e.innerHTML="",t.length===0){e.innerHTML=`
+      <li class="empty-favorites">
+        It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.
+      </li>
+    `;return}const r=t.map(o=>d(o));(await Promise.all(r)).forEach(o=>{const a=l(o);e.insertAdjacentHTML("beforeend",a)})}function l(e){return`
+    <li class="exercises_item" id="${e._id}">
+      <div">Card-exercise
+      </div>
+    </li>
+  `}function f(){document.addEventListener("click",e=>{if(e.target.classList.contains("remove-favorite-btn")){const t=e.target.getAttribute("data-id");v(t),n()}})}function v(e){const r=i().filter(s=>s!==e);localStorage.setItem("favoriteExercises",JSON.stringify(r))}document.addEventListener("DOMContentLoaded",()=>{n(),f()});
 //# sourceMappingURL=favorites.js.map
