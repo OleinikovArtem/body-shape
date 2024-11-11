51e66b6a9ee193967e0eae97c6b0c0452a86c989
Bignichok <62079994+Bignichok@users.noreply.github.com>
2024-11-11 11:15:12 +0000

    Deploying to gh-pages from @ OleinikovArtem/body-shape@8da79e68a2d7c90bedb897acba8e1f25e55fc53e 🚀

--- favorites.js
+++ favorites.js
@@ -1,2 +1,2 @@
-import{i,h as e,j as a,l as t,k as n}from"./assets/menu-BdnAZTHz.js";import"./assets/vendor-BcxMf0ji.js";document.addEventListener("DOMContentLoaded",()=>{i(),e(),a(),t(),n()});
+import{i,h as e,j as a,l as t,k as n}from"./assets/menu-BCaZb3i3.js";import"./assets/vendor-BcxMf0ji.js";document.addEventListener("DOMContentLoaded",()=>{i(),e(),a(),t(),n()});
 //# sourceMappingURL=favorites.js.map
